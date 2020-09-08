5b06d555f04f89c85c1da18441018edb46f510ac
Aniruddh Parwal <aniruddhparwal@gmail.com>
2020-09-08 17:39:03 +0530

    Updates

---
+++ precache-manifest.4f10ace97cc7c8a9ae32b384ce62a59b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deca9e49a6056fbf2f61c7ef507f04a8",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "49044cd4a98a3dd2c60b",
+    "url": "/amazon-clone/static/css/main.83bad626.chunk.css"
+  },
+  {
+    "revision": "308365cd669e77f2a7e2",
+    "url": "/amazon-clone/static/js/2.328bb88c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.328bb88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49044cd4a98a3dd2c60b",
+    "url": "/amazon-clone/static/js/main.88ab933e.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
